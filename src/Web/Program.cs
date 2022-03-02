d50ff67458e74b519f3d1c762eb184cdddcaaffe
omerfe <omerfe.eker@gmail.com>
2022-03-02 21:53:38 +0300

    products seed

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,19 +1,21 @@
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Web
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var storeContext = scope.ServiceProvider.GetRequiredService<StoreContext>();
+                await StoreContextSeed.SeedAsync(storeContext);
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
